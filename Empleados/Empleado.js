951632a6f7bf2b9f42f5aedc3c91194e415715a9
Facundo Andrés De Los Rios <facujofre96@gmail.com>
2022-09-01 18:43:49 -0300

    Fase 4 terminada utilizando concepto de polimorfismo para gestion de claves de empleados

--- Empleados/Empleado.js
+++ Empleados/Empleado.js
@@ -2,10 +2,21 @@ export class Empleado{
     #Nombre;
     #Dni;
     #Salario
+    #clave;
+
     constructor(Nombre, Dni, Salario){
         this.#Nombre = Nombre;
         this.#Dni = Dni;
         this.#Salario = Salario;
+        this.#clave = '';
+    }
+
+    asignarCLave(clave){
+        this.#clave = clave;
+    }
+
+    autenticable(clave){
+        return clave == this.#clave;
     }
 
     verBonificacion(){
--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-import { CuentaAhorro } from './CuentaAhorro.js';
-import { CuentaCorriente } from './CuentaCorriente.js';
-import { CuentaNomina } from './CuentaNomina.js';
 import { Director } from './Empleados/Director.js';
 import { Empleado } from './Empleados/Empleado.js';
 import { Gerentes } from './Empleados/Gerente.js';
+import { SistemaAutenticacion } from './SistemaAutenticacion.js';
 
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cliente2 = new Cliente('María','16979808','8989');
-
+/* 
 const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
 const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
 
 const cuentaAhorroLeonardo = new CuentaAhorro(cliente, '9985', '001', 0)
 
-const cuentaNominaLeonardo = new CuentaNomina(cliente,'9854','001',100);
+const cuentaNominaLeonardo = new CuentaNomina(cliente,'9854','001',100); */
 
 const empleado = new Empleado('Juan', '12319834', 10000);
 const gerente = new Gerentes('Pepe', '1123434', 12000);
 const director = new Director('Matias', '43200239', 15000);
 
-console.log(empleado.verBonificacion());
-console.log(gerente.verBonificacion());
-console.log(director.verBonificacion());
+empleado.asignarCLave('12345');
+gerente.asignarCLave('22222');
+director.asignarCLave('33333');
+
+
+console.log(SistemaAutenticacion.login(empleado, 12345));
+console.log(SistemaAutenticacion.login(gerente, 123444));
+console.log(SistemaAutenticacion.login(director, 33333));
