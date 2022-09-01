b66f1aadaff5907ebf1438ac7b6d6ce5b38df5d6
Facundo Andrés De Los Rios <facujofre96@gmail.com>
2022-09-01 12:14:50 -0300

    2da parte terminada con herencia de clases usando palabra clave super

--- Cuenta.js
+++ Cuenta.js
@@ -9,13 +9,27 @@ export class Cuenta{
         this.#saldo = saldo;
     }
 
+    set cliente(valor) {
+        if (valor instanceof Cliente)
+            this.#cliente = valor;
+    }
+
+    get cliente() {
+        return this.#cliente;
+    }
+
     depositoEnCuenta(valor) {
         if (valor > 0)
             this.#saldo += valor;
         return this.#saldo;
     }
 
-    retirarDeCuenta(valor) {
+    retirarDeCuenta(valor){
+        _retirarDeCuenta(valor, 0);
+    }
+
+    _retirarDeCuenta(valor, comision) {
+        valor = valor * (1+comision/100);
         if (valor <= this.#saldo)
             this.#saldo -= valor;
         return this.#saldo;
